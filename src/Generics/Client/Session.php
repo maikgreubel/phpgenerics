ad77f1171076ff53ba20997fc1f96d1153daf47e
Maik Greubel <greubel@nkey.de>
2015-10-03 13:10:59 +0100

    Remove local repository

---
+++ src/Generics/Client/Session.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of the PHP Generics package.
+ *
+ * @package Generics
+ */
+namespace Generics\Client;
+
+/**
+ * This class provides session provider
+ *
+ * @author Maik Greubel <greubel@nkey.de>
+ *
+ */
+class Session
+{
+    private $sessionId;
+
+    /**
+     * Create a new session provider
+     */
+    public function __construct()
+    {
+    }
+
+    /**
+     * Create session
+     */
+    public function create()
+    {
+        session_start();
+        $this->sessionId = session_id();
+    }
+
+    /**
+     * Destroy session
+     */
+    public function destroy()
+    {
+        session_destroy();
+    }
+
+    /**
+     * Add a new identifier with corresponding value to session storage
+     *
+     * @param string $key
+     * @param string $value
+     */
+    public function put($key, $value)
+    {
+        $_SESSION[$key] = $value;
+    }
+
+    /**
+     * Retrieve value from session storage for corresponding key
+     *
+     * @param string $key
+     * @return NULL|string
+     */
+    public function get($key)
+    {
+        if (!isset($_SESSION[$key])) {
+            return null;
+        }
+
+        return $_SESSION[$key];
+    }
+}
---
+++ src/Generics/Util/BasicAuth.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * This file is part of the PHP Generics package.
+ *
+ * @package Generics
+ */
+namespace Generics\Util;
+
+use Generics\GenericsException;
+use Generics\Client\HttpStatus;
+
+/**
+ * This class provides http basic auth provider
+ *
+ * @author Maik Greubel <greubel@nkey.de>
+ *
+ */
+class BasicAuth
+{
+    /**
+     * The basic auth user
+     *
+     * @var string
+     */
+    private $user;
+
+    /**
+     * The basic auth password
+     *
+     * @var string
+     */
+    private $password;
+
+    /**
+     * The list of files to skip authentication
+     *
+     * @var array
+     */
+    private $whitelist;
+
+    /**
+     * Realm name
+     *
+     * @var string
+     */
+    private $realm;
+
+    /**
+     * Create a new basic auth instance
+     *
+     * @param string $user The username
+     * @param string $password The password
+     * @param array $whitelist The list of files to skip authentication
+     * @param string $realm The name of the realm
+     */
+    public function __construct($user, $password, $whitelist = array(), $realm = "Authentication realm")
+    {
+        $this->user = $user;
+        $this->password = $password;
+        $this->whitelist = $whitelist;
+        $this->realm = $realm;
+    }
+
+    /**
+     * Perform authentication
+     *
+     * @param string $user The name the user provided
+     * @param string $password The password the user provided
+     * @param string $file
+     * @throws GenericsException
+     * @return boolean
+     */
+    public function auth($file = '')
+    {
+        if (php_sapi_name() == 'cli') {
+            throw new GenericsException("CLI does not support basic auth!");
+        }
+
+        if ($file && in_array($file, $this->whitelist)) {
+            return true;
+        }
+
+        $user = null;
+        $password = null;
+
+        if (isset($_SERVER['PHP_AUTH_USER'])) {
+            $user = $_SERVER['PHP_AUTH_USER'];
+        }
+        if (isset($_SERVER['PHP_AUTH_PW'])) {
+            $password = $_SERVER['PHP_AUTH_PW'];
+        }
+
+        if ($user && $password && $user == $this->user && $password == $this->password) {
+            return true;
+        }
+
+        $httpStatus = new HttpStatus(401, '1.0');
+        header('WWW-Authenticate: Basic realm=' . $this->realm);
+        header(sprintf('HTTP/%s', $httpStatus));
+        echo "Forbidden!";
+        return false;
+    }
+}
