039dcf060ccf9b81b9f2e333a91939bd3ce45262
Maik Greubel <greubel@nkey.de>
2017-09-01 18:05:38 +0200

    Fix issues and documentation

--- src/Generics/Util/ExceptionErrorHandler.php
+++ src/Generics/Util/ExceptionErrorHandler.php
@@ -1,4 +1,10 @@
 <?php
+
+/**
+ * This file is part of the PHP Generics package.
+ *
+ * @package Generics
+ */
 namespace Generics\Util;
 
 use Generics\DeprecatedException;
@@ -11,35 +17,53 @@ use Generics\UserWarningException;
 use Generics\WarningException;
 use ErrorException;
 
+/**
+ * This class provides a custom error handler for exception handling rather than internal error handling
+ *
+ * @author Maik Greubel <greubel@nkey.de>
+ *        
+ */
 class ExceptionErrorHandler
 {
+
     public function __construct()
     {
         $this->setErrorHandler();
     }
-    
+
     private function setErrorHandler()
     {
-        set_error_handler(array($this, 'errorHandler'), E_ALL | E_STRICT);
+        set_error_handler(array(
+            $this,
+            'errorHandler'
+        ), E_ALL | E_STRICT);
     }
-    
-    public function errorHandler(int $err_severity, string $err_msg, string $err_file, int $err_line, array $err_context)
+
+    public function errorHandler(int $err_severity, string $err_msg, string $err_file, int $err_line)
     {
-        if (0 === error_reporting()) { 
+        if (0 === error_reporting()) {
             return false;
         }
         
-        switch($err_severity)
-        {
-            case E_WARNING:  throw new WarningException  ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_NOTICE:  throw new NoticeException  ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_USER_ERROR:  throw new UserErrorException  ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_USER_WARNING:  throw new UserWarningException  ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_USER_NOTICE:  throw new UserNoticeException  ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_RECOVERABLE_ERROR:  throw new RecoverableErrorException ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_DEPRECATED:  throw new DeprecatedException  ($err_msg, 0, $err_severity, $err_file, $err_line);
-            case E_USER_DEPRECATED:  throw new UserDeprecatedException  ($err_msg, 0, $err_severity, $err_file, $err_line);
-            default:            throw new ErrorException  ($err_msg, 0, $err_severity, $err_file, $err_line);
+        switch ($err_severity) {
+            case E_WARNING:
+                throw new WarningException($err_msg, 0, $err_severity, $err_file, $err_line);
+            case E_NOTICE:
+                throw new NoticeException($err_msg, 0, $err_severity, $err_file, $err_line);
+            case E_USER_ERROR:
+                throw new UserErrorException($err_msg, 0, $err_severity, $err_file, $err_line);
+            case E_USER_WARNING:
+                throw new UserWarningException($err_msg, 0, $err_severity, $err_file, $err_line);
+            case E_USER_NOTICE:
+                throw new UserNoticeException($err_msg, 0, $err_severity, $err_file, $err_line);
+            case E_RECOVERABLE_ERROR:
+                throw new RecoverableErrorException($err_msg, 0, $err_severity, $err_file, $err_line);
+            case E_DEPRECATED:
+                throw new DeprecatedException($err_msg, 0, $err_severity, $err_file, $err_line);
+            case E_USER_DEPRECATED:
+                throw new UserDeprecatedException($err_msg, 0, $err_severity, $err_file, $err_line);
+            default:
+                throw new ErrorException($err_msg, 0, $err_severity, $err_file, $err_line);
         }
     }
 }
